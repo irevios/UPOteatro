1ae3ff9118f3c0e2a03859f378fffab800d0acf9
irevios <irevios@gmail.com>
2020-01-31 10:44:36 +0100

    fix Error

--- js/modificarVista.js
+++ js/modificarVista.js
@@ -199,7 +199,8 @@ function cambiaPrecioEntrada() {
         document.querySelector("#personasGrupal").value = personas;
         coefButaca = personas;
     } else {
-        let butacaSeleccionada = document.querySelector("#butacasRepresentadas .seleccionada").dataset.butaca.split("-");
+        if (elementoExiste("#butacasRepresentadas .seleccionada"))
+            let butacaSeleccionada = document.querySelector("#butacasRepresentadas .seleccionada").dataset.butaca.split("-");
         coefButaca = teatro.buscaButaca(butacaSeleccionada[0], butacaSeleccionada[1], butacaSeleccionada[2]).coefPrecio;
     }
     document.querySelector("#totalEntrada").value = (parseFloat(representacion.precioBase) * parseFloat(coefButaca)).toFixed(2);
