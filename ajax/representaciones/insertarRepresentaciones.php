13627ea69f8d0d917d3ac6eb40514512ef384dac
irevios <irevios@gmail.com>
2020-02-27 20:47:16 +0100

    Insertar representaciones

--- ajax/representaciones/insertarRepresentaciones.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Creamos la conexión al servidor.
-$conexion = mysqli_connect("localhost", "root", "", "upoteatro") or die(mysqli_error($conexion));
-mysqli_set_charset($conexion, "utf8");
-
-?>
